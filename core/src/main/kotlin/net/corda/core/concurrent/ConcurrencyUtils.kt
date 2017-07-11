79c62351f9a0ecb32e2f9e85a3dc242a953302d0
Andrzej Cichocki <andrzej.cichocki@r3.com>
2017-07-11 11:31:53 +0100

    consistent naming, restore kdoc

--- core/src/main/kotlin/net/corda/core/concurrent/ConcurrencyUtils.kt
+++ core/src/main/kotlin/net/corda/core/concurrent/ConcurrencyUtils.kt
@@ -14,14 +14,14 @@ import java.util.concurrent.atomic.AtomicBoolean
  * The result of the handler is copied into the result future, and the handler isn't invoked again.
  * If a given future errors after the result future is done, the error is automatically logged.
  */
-fun <S, T> firstOf(vararg futures: CordaFuture<S>, handler: (CordaFuture<S>) -> T) = firstOf(futures, defaultLog, handler)
+fun <V, W> firstOf(vararg futures: CordaFuture<V>, handler: (CordaFuture<V>) -> W) = firstOf(futures, defaultLog, handler)
 
 private val defaultLog = LoggerFactory.getLogger("net.corda.core.concurrent")
 @VisibleForTesting
 internal val shortCircuitedTaskFailedMessage = "Short-circuited task failed:"
 
-internal fun <S, T> firstOf(futures: Array<out CordaFuture<S>>, log: Logger, handler: (CordaFuture<S>) -> T): CordaFuture<T> {
-    val resultFuture = openFuture<T>()
+internal fun <V, W> firstOf(futures: Array<out CordaFuture<V>>, log: Logger, handler: (CordaFuture<V>) -> W): CordaFuture<W> {
+    val resultFuture = openFuture<W>()
     val winnerChosen = AtomicBoolean()
     futures.forEach {
         it.then {
@@ -39,14 +39,14 @@ internal fun <S, T> firstOf(futures: Array<out CordaFuture<S>>, log: Logger, han
 
 fun <V> Future<V>.get(timeout: Duration? = null): V = if (timeout == null) get() else get(timeout.toNanos(), TimeUnit.NANOSECONDS)
 
-/** Same as [Future.get] except that [ExecutionException] is unwrapped. */
+/** Same as [Future.get] but with a more descriptive name, and doesn't throw [ExecutionException], instead throwing its cause */
 fun <V> Future<V>.getOrThrow(timeout: Duration? = null): V = try {
     get(timeout)
 } catch (e: ExecutionException) {
     throw e.cause!!
 }
 
-fun <U, V> Future<U>.match(success: (U) -> V, failure: (Throwable) -> V): V {
+fun <V, W> Future<V>.match(success: (V) -> W, failure: (Throwable) -> W): W {
     return success(try {
         getOrThrow()
     } catch (t: Throwable) {
